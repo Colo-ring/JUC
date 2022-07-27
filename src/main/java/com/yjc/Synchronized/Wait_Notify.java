92d58b215ce0bc9d885ea2a3ee495697501364a1
coloring <1429157063@qq.com>
2022-07-27 22:20:45 +0800

    Day 2 - wait notify 的正确使用

---
+++ src/main/java/com/yjc/Synchronized/Wait_Notify.java
@@ -0,0 +1,72 @@
+package com.yjc.Synchronized;
+
+import com.yjc.util.Sleeper;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author IntelliYJC
+ * @create 2022/7/27 22:03
+ * 正确使用 wait notify 以及 sleep的方式
+ * 场景：小南必须要有烟才干活，没有烟就不干活一直等待
+ *      小女必须等她的外卖到了才干活，不然就一直等待
+ */
+@Slf4j(topic = "c.Wait_Notify")
+public class Wait_Notify {
+    static final Object room = new Object();
+    static boolean hasCigarette = false;    //有没有烟
+    static boolean hasTakeout = false;
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (room) {
+                log.debug("有烟没？[{}]", hasCigarette);
+                while (!hasCigarette) { //while防止虚假唤醒
+                    log.debug("没烟，先歇会！");
+                    try {
+                        room.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                log.debug("有烟没？[{}]", hasCigarette);
+                if (hasCigarette) {
+                    log.debug("可以开始干活了");
+                } else {
+                    log.debug("没干成活...");
+                }
+            }
+        }, "小南").start();
+
+        new Thread(() -> {
+            synchronized (room) {
+                Thread thread = Thread.currentThread();
+                log.debug("外卖送到没？[{}]", hasTakeout);
+                if (!hasTakeout) {
+                    log.debug("没外卖，先歇会！");
+                    try {
+                        room.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                log.debug("外卖送到没？[{}]", hasTakeout);
+                if (hasTakeout) {
+                    log.debug("可以开始干活了");
+                } else {
+                    log.debug("没干成活...");
+                }
+            }
+        }, "小女").start();
+
+        Sleeper.sleep(1000);
+        new Thread(() -> {
+            // 这里能不能加 synchronized (room)？
+            synchronized (room) {
+                hasTakeout = true;
+                //log.debug("烟到了噢！");
+                log.debug("外卖到了噢！");
+                room.notifyAll();
+            }
+        }, "送外卖的").start();
+    }
+}

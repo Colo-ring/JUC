1c6519967d5a299768bc3bf0ddcd50d8ad53c7a0
coloring <1429157063@qq.com>
2022-08-13 22:15:49 +0800

    享元模式，自定义数据库连接池。

--- src/test/java/TestFlyWeight.java
+++ src/test/java/TestFlyWeight.java
@@ -7,6 +7,11 @@ import java.sql.Connection;
 /**
  * @author IntelliYJC
  * @create 2022/8/13 22:09
+ * TODO:
+ * 1、连接动态增长和收缩
+ * 2、连接保活（可用性检测）
+ * 3、等待超时处理
+ * 4、分布式 hash
  */
 
 @Slf4j(topic = "c.TestFlyWeight")
